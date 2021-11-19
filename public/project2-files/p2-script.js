0ee9071aa86b5649d196fbd04007df7072326dc3
titusss <26855197+titusss@users.noreply.github.com>
2021-11-18 22:09:04 -0800

    Fixed png by jpg replacement.

--- public/project2-files/p2-script.js
+++ public/project2-files/p2-script.js
@@ -1,7 +1,7 @@
-var lines = document.getElementById("line-wrap");
-var field = document.getElementById("menu-field");
+// var lines = document.getElementById("line-wrap");
+// var field = document.getElementById("menu-field");
 
-lines.onclick = function() {
-	lines.classList.toggle("active");
-	field.classList.toggle("active");
-}
+// lines.onclick = function() {
+// 	lines.classList.toggle("active");
+// 	field.classList.toggle("active");
+// }
